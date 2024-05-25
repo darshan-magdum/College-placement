30dbda38c1913178ffed648355ee7f7ab461b613
darshan.magdum <darshanmagdum79@gmail.com>
2024-05-25 11:51:24 +0530

    removed routes for dashobar navigationg to hom

--- college_portal/src/pages/Profile/ProfileAdmin.jsx
+++ college_portal/src/pages/Profile/ProfileAdmin.jsx
@@ -65,7 +65,7 @@ const ProfileAdmin = () => {
                 <div className="row">
                     <div className="col-lg-3">
                         <div className="d-flex flex-column flex-shrink-0 p-3 bg-light" style={{ width: '100%' ,height:"100%"}}>
-                            <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+                            <a href="#" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
                                 <svg className="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
                                 <span className="fs-6">Admin Dashboard</span>
                             </a>
