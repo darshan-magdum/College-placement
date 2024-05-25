2ce1db226c4e455eb867967a98de2781e96d98f0
darshan.magdum <darshanmagdum79@gmail.com>
2024-05-25 14:53:45 +0530

    criteria

--- college_portal/src/App.js
+++ college_portal/src/App.js
@@ -1,16 +1,32 @@
-import './App.css';
-import {RouterProvider} from 'react-router-dom'
-import router from './router/config';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './componets/Navbar/Navbar';
 import Footer from './componets/Footer/Footer';
+import { About, Home } from './pages';
+import ProfileUser from './pages/Profile/ProfileUser';
+import ProfileAdmin from './pages/Profile/ProfileAdmin';
+import { Login } from './pages/Account/Login';
+import { Signup } from './pages/Account/Signup';
+import { Reset } from './pages/Account/Reset';
+import { Forget } from './pages/Account/Forget';
 
 function App() {
   return (
-    <div className="App">
-     <RouterProvider router={router}>
-<Footer/>
-     </RouterProvider>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Forget" element={<Forget />} />
+          <Route path="/Reset" element={<Reset />} />
+          <Route path="/profile/:id" element={<ProfileUser />} />
+          <Route path="/profile/admin" element={<ProfileAdmin />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
