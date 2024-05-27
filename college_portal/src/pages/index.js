13fa6d575f7d06b17bdd3eae0ad547330fecdd55
Pratik-109 <144760848+Pratik-109@users.noreply.github.com>
2024-05-27 22:48:34 +0530

    Delete college_portal directory

--- college_portal/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import Home from "./Home/Home";
-import About from "./About/About";
-export{
-    Home,
-    About
-}
--- college_portal/src/router/keys.js
+++
@@ -1,8 +0,0 @@
-export const DEFAULT = '/';
-export const ABOUT = '/about';
-export const LOGIN = '/Login';
-export const SIGNUP = '/Signup';
-export const FORGET = '/Forget';
-export const RESET = '/Reset';
-export const UPROFILE = '/profile/:id';
-export const APROFILE = '/profile/admin';
